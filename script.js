a74e469862768ba9693b0bcce601b5f24c3184ca
jacobfielder <114694870+jacobfielder@users.noreply.github.com>
2025-02-27 01:40:32 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// some javascript
+$(document).ready(function() { alert('hello!'); });
+
+// easter egg & console interaction
+console.log("Welcome to the console!");
+console.log("Try typing: roarlions()");
+
+function roarlions() {
+    console.log("🦁 You found the message! ROAR LIONS! 🦁");
+    console.log("You're officially a console explorer. 🚀");
+}
